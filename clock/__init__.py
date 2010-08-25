d300d96c35d6cb36197f2791b10555b760d19585
Johan Brinch <brinchj@gmail.com>
2010-08-25 21:02:49 +0200

    Use datetime in clock

--- clock/__init__.py
+++ clock/__init__.py
@@ -4,6 +4,6 @@ import time
 
 
 def now(diff=0):
-    delta = datetime.now() - datetime.fromtimestamp(0)
+    delta = datetime.utcnow() - datetime.fromtimestamp(0)
     now = Dec(delta.days*3600*24) + Dec(delta.seconds) + Dec(delta.microseconds)/1000000
     return now - diff
--- ntp/__init__.py
+++ ntp/__init__.py
@@ -1,11 +1,12 @@
 from socket import socket, AF_INET, SOCK_DGRAM, timeout
 from decimal import Decimal as Dec
+import clock
 import struct, sys, time
 
 TIME1970 = 2208988800L
 
 def get_time():
-    NTP_SERVER = "130.225.96.8"
+    NTP_SERVER = '130.225.96.8'
 
     client = socket( AF_INET, SOCK_DGRAM )
     data = '\x1b' + 47 * '\0'
@@ -13,10 +14,10 @@ def get_time():
 
     for i in xrange(20):
         try:
-            start = time.time()
+            start = clock.now()
             client.sendto( data, ( NTP_SERVER, 123 ))
             data, address = client.recvfrom( 1024 )
-            time_used = time.time() - start
+            time_used = clock.now() - start
             break
         except timeout:
             time.sleep(.1*i)
@@ -24,24 +25,21 @@ def get_time():
 
     if data:
         s = struct.unpack( '!12I', data )
-        return start, time_used, s[10], s[11]
+        return start, time_used, Dec(s[10]), Dec(s[11])
 
 
 def get_time_exact():
     time_used = 1
-    while time_used > 0.0015:
+    while time_used > Dec(str(0.0015)):
         time_start, time_used, secs, frac = get_time()
-    his = Dec(str(time_start)) + Dec(str(time_used))/2
-    her = Dec(secs) + (Dec(frac) / Dec(2**32)) + Dec(str(time_used))/2 - TIME1970
+    his = time_start + time_used/2
+    her = secs + (frac / Dec(2**32)) + time_used/2 - TIME1970
     return his - her
 
 def get_time_more_exact():
     tries = 10
-    
     msum = Dec(0)
-
     for n in xrange(tries):
         msum += get_time_exact()
         time.sleep(0.005)
-
     return msum / tries
