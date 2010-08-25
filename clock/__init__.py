4f46d2a417a6326fab857d4817a943e81ec3dce8
Mathias Svensson <freaken@freaken.dk>
2010-08-25 21:00:43 +0200

    Added run_at and build_clock

--- clock/__init__.py
+++ clock/__init__.py
@@ -7,3 +7,16 @@ def now(diff=0):
     delta = datetime.now() - datetime.fromtimestamp(0)
     now = Dec(delta.days*3600*24) + Dec(delta.seconds) + Dec(delta.microseconds)/1000000
     return now - diff
+
+def build_clock(diff):
+    return lambda: now(diff)
+
+def run_at(t, fun, clock):
+    while clock()+Dec('0.02') < t:
+        time.sleep(0.001)
+
+    while clock() < t:
+        pass
+
+    fun()
+
