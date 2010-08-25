59ceb881e387a2755453032acc84876bf474381c
Johan Brinch <brinchj@gmail.com>
2010-08-26 00:44:05 +0200

    Test script for timing overhead in media player.

---
+++ time_overhead.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import sys, subprocess
+import ntp, clock, audio
+
+from decimal import Decimal
+
+path = sys.argv[1]
+diff = ntp.get_time_more_exact()
+real_clock = clock.build_clock(diff)
+
+CMD = ['mplayer', '-endpos', '5', path]
+
+def player():
+    print 'play:', real_clock()
+    subprocess.call(CMD)
+    print 'done:', real_clock()
+
+clock.run_at(real_clock(), player, real_clock)
