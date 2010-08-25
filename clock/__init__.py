f5c5d859872b0e78440aad15491c56efc6d0b892
Johan Brinch <brinchj@gmail.com>
2010-08-25 21:40:33 +0200

    Beatiful clock

--- clock/__init__.py
+++ clock/__init__.py
@@ -8,15 +8,14 @@ def now(diff=0):
     now = Dec(delta.days*3600*24) + Dec(delta.seconds) + Dec(delta.microseconds)/1000000
     return now - diff
 
-def build_clock(diff):
+def build_clock(diff=0):
     return lambda: now(diff)
 
-def run_at(t, fun, clock):
-    while clock()+Dec('0.02') < t:
+def run_at(start_time, fun, clock):
+    critical = Dec('0.02')
+    while clock() + critical < start_time:
         time.sleep(0.001)
-
-    while clock() < t:
+    while clock() < start_time:
         pass
-
     fun()
 
